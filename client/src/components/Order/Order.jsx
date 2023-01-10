1ee9f857d5c76cb814cbec601da3c743452e908f
Roman Viktorovich <roman.viktorovich@outlook.com>
2023-01-10 18:00:02 +0300

    refactor order two blocks

--- client/src/components/Order/Order.jsx
+++ client/src/components/Order/Order.jsx
@@ -20,8 +20,26 @@ export function Order() {
                 </header>
                 <main>
                     <h2 className={s.headerLine}>Заказ на доставку</h2>
-                    <div className={"order__holder"}>
-                        <div className={"order__inputFields"}>
+                    <div className={s.order__holder}>
+                        <div className={s.order__inputFields}>
+
+                            <div className={s.order__items}>
+                                <label htmlFor="name">Имя:</label>
+                                <input type="text" id="name" name="name"/>
+                            </div>
+                            <div className={s.order__items}>
+                                <label  for="phone" >Номер телефона:</label>
+                                <input  type="tel" id="phone" name="phone" pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}"/>
+                            </div>
+
+                            <div className={`${s.order__items}  ${s.order__items__big}` }>
+                                <label htmlFor="deliver">Куда доставить:</label>
+                                <input type="text" id="deliver" name="deliver"/>
+                            </div>
+                            <div className={s.order__wraperBtn}>
+                                <button className={`${s.btn} ${s.btn__gray}`}>  <span> &#65308;</span> Вернуться в меню  </button>
+                                <button className={`${s.btn} ${s.btn__orange}`}>Оформить заказ <span> &#65310;</span>  </button>
+                            </div>
 
                             </div>
                         <div className={s.aside}>
@@ -32,22 +50,22 @@ export function Order() {
                                             <p className={s.title}>Coca-Cola</p>
                                             <p className={s.title}>Coca-Cola</p>
                                         </div>
-
                                         <p className={s.price}>3 × 75 ₽</p>
                                     </div>
                                     <span className={s.massProduct}>0,5 л</span>
                                 </li>
                                 <li>
-                                    <div className={s.food}>
-                                        <p className={s.title}>Рулетики с ананасами + <br/>Рулетики с брусникой, 16 шт
-                                        </p>
+                                    <div className={s.wrapper__food}>
+                                        <div className="food">
+                                            <p className={s.title}>Рулетики с ананасами</p>
+                                            <p className={s.title}>Рулетики с брусникой</p>
+                                        </div>
                                         <p className={s.price}>195 ₽</p>
                                     </div>
                                     <span className={s.massProduct}>16 шт</span>
                                 </li>
                             </ul>
                         </div>
-
                     </div>
                 </main>
                 <Footer/>
