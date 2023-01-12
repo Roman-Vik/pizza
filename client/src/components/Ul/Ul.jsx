9f233fa80ae831c12e19e6fa874207c008b92ea9
Artem <115172694+ArtemEgorov007@users.noreply.github.com>
2023-01-12 14:01:30 +0300

    first map

--- client/src/components/Ul/Ul.jsx
+++ client/src/components/Ul/Ul.jsx
@@ -1,21 +1,27 @@
-import s from "./Ul.module.css"
+import s from "./Ul.module.css";
 
-export function Ul({data}) {
+export function Ul({ data }) {
+	console.log(data);
 
-console.log(data)
-
-    return (
-        <nav>
-            <ul className={s.menu__list}>
-                {
-                   data?.list?.map( (el,i)=> <li className={s.menu__item}>
-                        <a href="src/components/Ul/Ul#"
-                           className={s.menu__link}>{el}</a></li> )
-                }
-                <li className={s.menu__item}>
-                    <a href="src/components/Ul/Ul#"
-                       className={s.menu__link}>Другие товары</a></li>
-            </ul>
-        </nav>
-    )
+	return (
+		<nav>
+			<ul className={s.menu__list}>
+				{data?.list?.map((el, i) => (
+					<li className={s.menu__item}>
+						<a
+							href="src/components/Ul/Ul#"
+							className={s.menu__link}
+						>
+							{el}
+						</a>
+					</li>
+				))}
+				<li className={s.menu__item}>
+					<a href="src/components/Ul/Ul#" className={s.menu__link}>
+						Другие товары
+					</a>
+				</li>
+			</ul>
+		</nav>
+	);
 }
