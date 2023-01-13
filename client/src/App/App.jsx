471fff86ffb1fae7da782bb437634ef73cdcb714
m3tel <m3tel161@mail.ru>
2023-01-13 16:23:29 +0300

    add Pages, router

--- client/src/App/App.jsx
+++ client/src/App/App.jsx
@@ -9,18 +9,25 @@ import {FooterCard} from "../UI_Cards/FooterCard/FooterCard";
 import { Home } from '../Pages/Home';
 import { NotFound } from '../Pages/NotFound';
 import { Pizza } from '../Pages/Pizza';
+import { Layout } from '../layout/Layout';
+import { Basket } from '../components/Basket/Basket';
+import { BasketPages } from '../Pages/Basket/BasketPages';
+import { FoodOrdering } from '../Pages/FoodOrdering/FoodOrdering';
+import { Accepted } from '../Pages/Accepted/Accepted';
 
 
 export function App() {
     return (
         <>
-        <Header/>
          <Routes>
             <Route path="/" element={<Home></Home>}></Route>
+            {/* <Route path='/' element={<Layout></Layout>}></Route> */}
+            <Route path='/basket' element={<BasketPages></BasketPages>}></Route>
+            <Route path='/order' element={<FoodOrdering></FoodOrdering>}></Route>
+            <Route path='/accepted' element={<Accepted></Accepted>}></Route>
             <Route path="/pizza" element={<Pizza></Pizza>}></Route>
             <Route path="*" element={<NotFound></NotFound>}></Route>
          </Routes>
-        <Footer/>
         </>
     );
 }
--- client/src/layout/Layout.jsx
+++ client/src/layout/Layout.jsx
@@ -1,5 +1,23 @@
-function Layout({children}) {
+import React from 'react'
+import s from "./Layout.module.css"
+import { Header } from '../UI/Header/Header'
+import { Ul } from '../components/Ul/Ul'
+import { Button } from '../components/Button/Button'
+import { Footer } from '../UI/Footer/Footer'
+import { Container } from '../components/Container/Container'
 
-    {children}
 
+export function Layout ({data}) {
+  return (
+    <>
+    <Header></Header>
+    <Container>
+     <div className={s.menu}>
+        <Ul data={data}></Ul>
+        <Button></Button>
+     </div>
+     </Container>
+     <Footer></Footer>
+    </>
+  )
 }
