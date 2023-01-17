cc7b3827e1f3b427cece4b1f3dc4c287ca7d5526
Roman Viktorovich <roman.viktorovich@outlook.com>
2023-01-17 17:08:37 +0300

    ref server 17:08   17/01

--- server/index.js
+++ server/index.js
@@ -1,10 +1,16 @@
 import express, {request} from "express"
 import cors from "cors"
-import data from './data.json' assert {type: 'json'};
+import data from './srs/data.json' assert {type: 'json'};
+import drinks from './srs/pages/drinks.json' assert {type: 'json'};
+import pizza from './srs/pages/pizzas.json' assert {type: 'json'};
+import combo from './srs/pages/combo.json' assert {type: 'json'};
+import snacks from './srs/pages/snacks.json' assert {type: 'json'};
+import desserts from './srs/pages/dessert.json' assert {type: 'json'};
+import other from './srs/pages/other.json' assert {type: 'json'};
 
 const app =  express()
 
-const  host = '127.0.0.1'
+const host = '127.0.0.1'
 const port =  5000
 
 app.use(express.json())
@@ -15,21 +21,27 @@ app.use(
     express.static(`./assets/images/`)
 );
 
-
-app.get('/home', (req, res)=>{
-    res.status(200).type('text/plain')
-    res.send('Home page')
-})
-
-app.get('/pizza', (req, res)=>{
-    res.status(200).type('text/plain')
-    res.send('Pizza')
-})
 app.get('/api', (req, res)=>{
     return res.status(201).json(data)
 })
-
-
+app.get('/pizzas', (req, res)=>{
+    return res.status(201).json(pizza)
+})
+app.get('/combo', (req, res)=>{
+    return res.status(201).json(combo)
+})
+app.get('/snacks', (req, res)=>{
+    return res.status(201).json(snacks)
+})
+app.get('/desserts', (req, res)=>{
+    return res.status(201).json(desserts)
+})
+app.get('/drinks', (req, res)=>{
+    return res.status(201).json(drinks)
+})
+app.get('/other', (req, res)=>{
+    return res.status(201).json(other)
+})
 
 app.listen(port,host, ()=>{
     console.log(`Server listens http://${host}:${port}`)
