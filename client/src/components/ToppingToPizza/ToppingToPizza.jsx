907341753c2c2c316047b5f40a2c63025895d2d5
Roman Viktorovich <roman.viktorovich@outlook.com>
2023-01-18 18:25:16 +0300

    refactor input size pizza 18:26

--- client/src/components/ToppingToPizza/ToppingToPizza.jsx
+++ client/src/components/ToppingToPizza/ToppingToPizza.jsx
@@ -1,12 +1,12 @@
 import s from "./ToppingToPizza.module.css"
 import {Container} from "../Container/Container";
 import x from "../../components/images/Vector.svg"
+import img from "../../components/images/Ingredients/image_1.png"
 import {PizzaSelection} from "../PizzaSelection/PizzaSelection";
 
+
+
 export function ToppingToPizza() {
-
-
-
     return (
         <Container>
             <div className={s.topping}>
@@ -17,107 +17,47 @@ export function ToppingToPizza() {
                     </div>
                 </div>
                 <div className={s.right_section}>
-                <h2 className={s.right_section__headLine}>Чиззи чеддер</h2>
+                    <h2 className={s.right_section__headLine}>Чиззи чеддер</h2>
                     <p className={s.right_section__text}>{`30 см, традиционное тесто, 480 г`}</p>
                     <div className={s.right_sectiont__selected}>
-                        <div className={s.right_sectiont__name}>чеддер<button ><img src={x} alt="x"/></button>,</div>
-                        <div className={s.right_sectiont__name}>чеддер<button><img src={x} alt="x"/></button>,</div>
-                    </div>
-
-                    <div className={s.right_sectiont__choiceOfIngredients}>
-                       <div  className={s.right_section__radio}>
-                            <PizzaSelection/>
-                            <PizzaSelection/>
-                            <PizzaSelection/>
-                       </div>
-                        <div  className={s.right_section__radio}>
-                            <PizzaSelection/>
-                            <PizzaSelection/>
+                        <div className={s.right_sectiont__name}>чеддер
+                            <button><img src={x} alt="x"/></button>
+                            ,
+                        </div>
+                        <div className={s.right_sectiont__name}>чеддер
+                            <button><img src={x} alt="x"/></button>
+                            ,
                         </div>
                     </div>
 
-
-
-
-
-
-
-
-
-
-
-
+                    {/*<div className={s.right_sectiont__choiceOfIngredients}>*/}
+                    {/*    <div className={s.right_section__radio}>*/}
+                    {/*        <PizzaSelection/>*/}
+                    {/*        <PizzaSelection/>*/}
+                    {/*        <PizzaSelection/>*/}
+                    {/*    </div>*/}
+                    {/*    <div className={s.right_section__radio}>*/}
+                    {/*        <PizzaSelection/>*/}
+                    {/*        <PizzaSelection/>*/}
+                    {/*    </div>*/}
+                    {/*</div>*/}
                     <div className={s.wrapperIngredients}>
                         <h2>Добавить в пиццу</h2>
-            <div className={"все фотки пицц"}>
-                <div>
-                    <input
-                        type="radio"
-                        name="topping"
-                        value="Regular"
-                        id="regular"
-                        // checked={topping === "Regular"}
-                        // onChange={onOptionChange}
-                    />
-                    <label htmlFor="regular">Regular</label>
-                </div>
-                <div>
-                    <input
-                        type="radio"
-                        name="topping"
-                        value="Regular"
-                        id="regular"
-                        // checked={topping === "Regular"}
-                        // onChange={onOptionChange}
-                    />
-                    <label htmlFor="regular">Regular</label>
-                </div>
-                <div>
-                    <input
-                        type="radio"
-                        name="topping"
-                        value="Regular"
-                        id="regular"
-                        // checked={topping === "Regular"}
-                        // onChange={onOptionChange}
-                    />
-                    <label htmlFor="regular">Regular</label>
-                </div>
-                <div>
-                    <input
-                        type="radio"
-                        name="topping"
-                        value="Regular"
-                        id="regular"
-                        // checked={topping === "Regular"}
-                        // onChange={onOptionChange}
-                    />
-                    <label htmlFor="regular">Regular</label>
-                </div>
-                <div>
-                    <input
-                        type="radio"
-                        name="topping"
-                        value="Regular"
-                        id="regular"
-                        // checked={topping === "Regular"}
-                        // onChange={onOptionChange}
-                    />
-                    <label htmlFor="regular">Regular</label>
-                </div>
-                <div>
-                    <input
-                        type="radio"
-                        name="topping"
-                        value="Regular"
-                        id="regular"
-                        // checked={topping === "Regular"}
-                        // onChange={onOptionChange}
-                    />
-                    <label htmlFor="regular">Regular</label>
-                </div>
-            </div>
-
+                        <div className={"все фотки пицц"}>
+                            <div>
+                                <input
+                                    width="84px"
+                                    height="84px"
+                                    className={s.radio__inputImg}
+                                    src={img}
+                                    type="image"
+                                    name="Ingredients"
+                                    value="mushrooms"
+                                    id="regular"
+                                    onClick={e => console.log(e.target.value)}
+                                />
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
