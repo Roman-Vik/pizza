0b296703a18a69be5f0c29b1b906a4f14f805d3e
m3tel <m3tel161@mail.ru>
2023-01-19 14:28:01 +0300

    add new Pages, Ul fix

--- client/src/App/App.jsx
+++ client/src/App/App.jsx
@@ -1,5 +1,6 @@
 import './App.css';
 import {Routes, Route} from "react-router-dom"
+import { useState, useEffect } from "react";
 import { Home } from '../Pages/Home/Home';
 import { NotFound } from "../Pages/NotFound/NotFound"
 import { Pizza } from "../Pages/Pizza/Pizza"
@@ -10,11 +11,27 @@ import { Combo } from '../Pages/Combo/Combo';
 import { Snacks } from "../Pages/Snacks/Snacks"
 import { Desserts } from "../Pages/Desserts/Desserts"
 import { Drinks } from "../Pages/Drinks/Drinks"
-import { ModalCard } from '../components/ModalCard/ModalCard';
-import {ToppingToPizza} from "../components/ToppingToPizza/ToppingToPizza";
+import { OtherProducts } from '../Pages/OtherProducts/OtherProducts';
 
 
 export function App() {
+
+    const [data, setData] = useState({});
+	
+
+
+	useEffect(() => {
+		fetch("http://localhost:5000/api", {
+			method: "GET",
+			headers: {
+				"Content-Type": "application/json",
+				"Access-Control-Allow-Origin": "*"
+			}
+		})
+			.then((data) => data.json())
+			.then((d) => setData(d));
+	}, []);
+
 	return (
 		<>
 			<Routes>
@@ -34,6 +51,7 @@ export function App() {
 				<Route path="/snacks" element={<Snacks></Snacks>}></Route>
 				<Route path="/desserts" element={<Desserts></Desserts>}></Route>
 				<Route path="/drinks" element={<Drinks></Drinks>}></Route>
+				<Route path="/other_products" element={<OtherProducts></OtherProducts>}></Route>
 				<Route path="*" element={<NotFound></NotFound>}></Route>
 			</Routes>
 		</>
--- client/src/Pages/Pizza/Pizza.jsx
+++ client/src/Pages/Pizza/Pizza.jsx
@@ -1,20 +1,13 @@
 import React from 'react'
-
-import { Card } from '../../components/Card/Card'
-
-import { NameProduct } from '../../components/NameProduct/NameProduct'
 import { Footer } from '../../UI/Footer/Footer'
 import { Header } from '../../UI/Header/Header'
-import {Section} from "../../components/Section/Section";
+
 
 export function Pizza () {
   return (
     <>
     <Header></Header>
-    <div>ПИЦЦА</div>
     <Footer></Footer>
-    <Section/>
-    <Card></Card>
     </>
   )
 }
